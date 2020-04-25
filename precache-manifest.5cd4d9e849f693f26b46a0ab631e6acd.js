ab2670c4a799c8f9ca89a83f1f3c6db78fed137e
SkanderBM <skander.bmahmoud@gmail.com>
2020-04-25 17:37:36 +0200

    build

---
+++ precache-manifest.5cd4d9e849f693f26b46a0ab631e6acd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b22bf981b382db402a3c463dcf77db0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4c05581e401942cf1c0",
+    "url": "/static/css/2.c3343ac3.chunk.css"
+  },
+  {
+    "revision": "c51e5cdd586c6b12d02f",
+    "url": "/static/css/main.9301a56b.chunk.css"
+  },
+  {
+    "revision": "b4c05581e401942cf1c0",
+    "url": "/static/js/2.f63c2500.chunk.js"
+  },
+  {
+    "revision": "c111a90a094aacb40fe0b200d3894369",
+    "url": "/static/js/2.f63c2500.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c51e5cdd586c6b12d02f",
+    "url": "/static/js/main.4e04a7e0.chunk.js"
+  },
+  {
+    "revision": "a56d2b846561a1de49a8",
+    "url": "/static/js/runtime-main.66e18f5a.js"
+  }
+]);
